fd4d7c60e04d9a1d63e6b3bcf70d6fd9f8424535
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-01-30 20:54:45 +0000

    Added data() with value array to caputre selection

--- src/components/controls/MultipleChoiceInput.vue
+++ src/components/controls/MultipleChoiceInput.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container fluid>
     <label>{{label}}</label>
-    <v-checkbox v-for="item in options" :label="item" :value="item"></v-checkbox>
+      <v-checkbox v-for="item in options" v-model="value" :key="item" :label="item" :value="item"></v-checkbox>
+      <p> selected: {{ value }}</p>
   </v-container>
 </template>
 
 <script>
   export default {
     name: 'MultipleChoiceInput',
-    props: ['label', 'name', 'options']
+    props: ['label', 'name', 'options'],
+    data() {
+        return {
+            value: []
+        }
+    }
   }
 </script>
