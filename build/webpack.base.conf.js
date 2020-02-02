81e94d7898a74ef1fdfd03d9bdfbffefabf29cef
Benedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-02-02 17:20:19 +0000

    Fixing build (again)
    I'm like 80% sure I've got it this time

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,7 +21,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.Vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
