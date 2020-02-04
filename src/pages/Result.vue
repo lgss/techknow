e31e3696e08cacf7d9a74d4c7b0a390d0792735e
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-02-04 14:38:15 +0000

    Added page to render resource

---
+++ src/pages/Result.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'Result',
+    components: {},
+    props: ["resource"],
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
