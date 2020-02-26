3fca84b0f5a1093b1ab83c8f70f80e675fca2a20
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-02-26 14:40:22 +0000

    Update Stimulus.vue

--- src/components/controls/Stimulus.vue
+++ src/components/controls/Stimulus.vue
@@ -1,13 +1,20 @@
 <template>
   <v-container fluid>
-      <v-div>{{content}}</v-div>
+        <v-col :align='this.align'>
+            <v-row>
+                <v-label>{{label}}</v-label>
+            </v-row>
+            <v-row>
+                <div v-html="content"></div>
+            </v-row>
+      </v-col>
   </v-container>
 </template>
 
 <script>
   export default {
     name: 'Stimulus',
-    props: ['content', 'label', 'name'],
+    props: ['content', 'label', 'name','align'],
     data() {
         return {}
     }
