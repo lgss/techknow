8d4257418edfa7cf3d471699d71b3955838ecfcb
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-02-26 17:42:36 +0000

    Update Result.vue

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -1,23 +1,32 @@
 <template>
-  <v-container id="container-results">
-    <v-row v-for="resource in filteredList " :key="resource.name">
-      <v-col>
-          <v-card class="mx-auto" max-width="344">
-            <v-card-text>
-              <p class="display-1 text--primary"> {{ resource.name }}</p>
-              <div class="text--primary"> 
-                <span v-html="resource.content"></span>
-              </div>
-              <div> 
-                      <v-chip v-for="tag in resource.tags" :key="tag" class="ma-2">
-                        {{ tag }}
-                      </v-chip>
-              </div>
-            </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col align="left">
+          <v-btn id="btn-restart-assessment" @click.native="restart">Start Again</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container id="container-results">
+      <v-row v-for="resource in filteredList " :key="resource.name">
+        <v-col>
+            <v-card class="mx-auto" max-width="344">
+              <v-card-text>
+                <p class="display-1 text--primary"> {{ resource.name }}</p>
+                <div class="text--primary"> 
+                  <span v-html="resource.content"></span>
+                </div>
+                <div> 
+                  <v-chip v-for="tag in resource.tags" :key="tag" class="ma-2">
+                    {{ tag }}
+                  </v-chip>
+                </div>
+              </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -36,6 +45,11 @@ export default {
             })
         })
         return tags
+      },
+      restart() {
+        if(confirm("Are you sure you want to start again? The current results will be lost")){
+
+        }
       }
     },
     computed: {
