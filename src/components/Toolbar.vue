0d6f6c3d36339d1df7cdb6a27dc20525d0d6a392
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-04-27 17:13:05 +0100

    amended as link was not returning to root

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app-bar app :color="primary">
-        <a href='#/' style="text-decoration:none"><v-toolbar-title class="white--text"><b>{{title}}</b></v-toolbar-title></a>
+        <a href='/' style="text-decoration:none"><v-toolbar-title class="white--text"><b>{{title}}</b></v-toolbar-title></a>
         <v-spacer/>
     </v-app-bar>
 </template>
