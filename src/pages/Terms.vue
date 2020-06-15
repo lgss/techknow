7cc4c5467691755014d2d127957c614a113b2e59
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-06-15 15:46:30 +0100

    Update Terms.vue

--- src/pages/Terms.vue
+++ src/pages/Terms.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        terms
+        <v-progress-circular id="loading-terms" v-if="loading"></v-progress-circular>
+        <div v-else-if="terms" class='text-left'>
+            <h1>Terms and Conditions</h1>
+            <div id='terms-content' class='text-left'>
+                {{terms}}
+            </div>
+        </div>
+        <div v-else>We're sorry, but we're unable to find this at the moment.</div>
     </div>
 </template>
 
 <style>
+.terms-content {
+
+}
 
 </style>
 
@@ -13,11 +23,19 @@ export default {
     name: "terms",
     data() {
         return {
-            endpoint: process.env.VUE_APP_API_ENDPOINT
+            endpoint: process.env.VUE_APP_API_ENDPOINT,
+            terms: null,
+            loading: true
         }
     },
     created() {
-        fetch(this.endpoint + '/config/terms')
+        fetch(this.endpoint + '/config/disclaimer')
+        .then(x=>x.json())
+        .then(x=>{
+            this.terms = x;
+        })
+        .catch()
+        .finally(()=>this.loading = false)
     }
 }
 </script>
