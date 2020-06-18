2e02c7160e6e1537aa87a05de09c5e0b3f0bd4cd
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-06-18 17:16:49 +0100

    Update index.js
    Page header metadata

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,48 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from '@/pages/Home'
-import Assess from '@/pages/Assess'
-import Result from '@/pages/Result'
-import Select from '@/pages/Select'
+import Home from "@/pages/Home";
+import Assess from "@/pages/Assess";
+import Result from "@/pages/Result";
+import Select from "@/pages/Select";
 
-Vue.use(Router)
-
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/assess',
-      name: 'Assessment',
-      props: true,
-      component: Assess
-    },
-    {
-      path: '/select',
-      name: 'Select',
-      component: Select
-    },
-    {
-      path: '/result',
-      name: 'Result',
-      component: Result,
-      props: true
-    }
-  ]
-})
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: Home,
+            meta: {
+                title: "Hello and Welcome",
+            },
+        },
+        {
+            path: "/assess",
+            name: "Assessment",
+            props: true,
+            component: Assess,
+            meta: {
+                title: "Assess",
+            },
+        },
+        {
+            path: "/select",
+            name: "Select",
+            component: Select,
+            meta: {
+                title: "Choose your journey",
+            },
+        },
+        {
+            path: "/result",
+            name: "Result",
+            component: Result,
+            props: true,
+            meta: {
+                title: "Resources for you",
+            },
+        },
+    ],
+});
