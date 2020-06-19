739bc428f6e2d56172ab3d1712afbbe66059dc4c
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-06-19 15:43:51 +0100

    display a choice

---
+++ src/components/controls/Choice.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-item v-slot:default="{ active, toggle }" :value="index">
+        <v-card :color="active ? 'primary' : 'white lighten-3'" @click="toggle(); value.selected = !active">
+            <div class="d-flex flex-no-wrap align-center">
+                <v-avatar class="ma-3" size="125" tile>
+                    <v-icon x-large v-if="imgSrc === undefined">mdi-selection-ellipse</v-icon>
+                    <v-img v-else :src="imgSrc"></v-img> 
+                </v-avatar>
+                <div>
+                    <v-card-title  class="headline" v-text="label"></v-card-title>
+                    
+                </div>
+            </div>
+        </v-card>
+    </v-item>
+</template>
+
+<script>
+  export default {
+    name: 'Choice',
+    props: ['value', 'index', 'label', 'imgSrc']
+  }
+</script>
