2c16e8c65f62b567e582c013f209de4d54a65904
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-06-21 23:32:55 +0100

    Switch radio to choice for selection

--- src/components/controls/SingleChoiceInput.vue
+++ src/components/controls/SingleChoiceInput.vue
@@ -1,30 +1,38 @@
 <template>
   <v-container fluid>
-    <label>{{label}}</label>
-    <v-radio-group v-model="selectedChoice" @change="onChange" :rules="rules" :class="name">
-      <v-radio 
-        v-for="choice in choices" 
-        :key="choice.value" 
-        :label="choice.value" 
-        :value="choice"
-      ></v-radio>
-    </v-radio-group>
+    <div class="text-left">
+      <h2>{{label}}</h2>
+      <h3>Please select one choice</h3>
+    </div>
+    <v-item-group v-model="sel" @change="onChange" :rules="rules" :class="name">
+      <v-row dense>
+          <v-col v-for="choice in choices" :key="choice.value" :value="choice" cols="12">
+              <choice :value="choice" :index="choice" :label="choice.value"/>
+          </v-col>
+      </v-row>
+    </v-item-group>
   </v-container>
 </template>
 
 <script>
+  import Choice from '@/components/controls/Choice.vue'
+  
   export default {
     name: 'SingleChoiceInput',
     props: ['label', 'name', 'choices', 'isMandatory'],
+    components: {
+      Choice,
+    },
     data() {
         return {
-            selectedChoice: {},
+            sel: {},
             rules: this.isMandatory ? [choice => choice.value !== undefined || 'Please select a response'] : []
         }
     },
     methods: {
       onChange: function() {
-        this.$emit('responded', [this.selectedChoice])
+        console.log(this.sel)
+        this.$emit('responded', [this.sel])
       }
     }
   }
