5022fa84b0245069903057b7d1ab3258b87bf016
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-06-29 17:03:33 +0100

    add test file to check config works on pipeline

---
+++ tests/test_player.py
@@ -0,0 +1,19 @@
+from test_bootstrap import JerichoTest
+import unittest
+import time
+import random
+import json
+
+class PlayerTests(JerichoTest):
+
+    def test_one(self):
+        self.browser.get(self.env["root"])
+
+        heading = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(heading.text, 'Rekommend Management', 'Title check')
+
+    def test_two(self):
+        self.browser.get(self.env["root"])
+
+        tab = self.browser.find_element_by_class_name('v-tab--active')
+        self.assertEqual(tab.text, 'GENERAL', 'General tab check')        
