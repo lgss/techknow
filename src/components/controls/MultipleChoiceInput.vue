2ae592fdee372631d3957731db0be6483aebd497
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-07-07 20:30:16 +0100

    input wrapper

--- src/components/controls/MultipleChoiceInput.vue
+++ src/components/controls/MultipleChoiceInput.vue
@@ -1,39 +1,54 @@
 <template>
-  <v-container fluid>
-    <div class="text-left">
-      <h2>{{label}}</h2>
-      <h3>Please select one or more</h3>
-    </div>
-    <v-item-group multiple v-model="sel" @change="onChange" :rules="rules" :class="name">
-      <v-row dense>
-          <v-col v-for="choice in choices" :key="choice.value" :value="choice" cols="12">
-              <choice :value="choice" :index="choice" :label="choice.value"/>
-          </v-col>
-      </v-row>
-    </v-item-group>
-  </v-container>
-
+    <v-input :rules="rules" :value="sel">
+        <v-container fluid>
+            <div class="text-left">
+                <h2>{{ label }}</h2>
+                <h3>Please select one or more</h3>
+            </div>
+            <v-item-group v-model="sel" :class="name" multiple>
+                <v-row dense>
+                    <v-col
+                        v-for="choice in choices"
+                        :key="choice.value"
+                        cols="12"
+                    >
+                        <choice
+                            :value="choice"
+                            :index="choice"
+                            :label="choice.value"
+                        />
+                    </v-col>
+                </v-row>
+            </v-item-group>
+        </v-container>
+    </v-input>
 </template>
 
 <script>
-  import Choice from '@/components/controls/Choice.vue'
-  export default {
-    name: 'MultipleChoiceInput',
-    props: ['label', 'name', 'choices', 'isMandatory'],
+import Choice from "@/components/controls/Choice.vue";
+
+export default {
+    name: "MultipleChoiceInput",
+    props: ["label", "name", "choices", "isMandatory"],
     components: {
-      Choice,
+        Choice,
     },
     data() {
         return {
             sel: [],
-            rules: this.isMandatory ? [value => value.length > 0 || 'Please select at least one response'] : []
-        }
+            rules: this.isMandatory
+                ? [
+                      (value) =>
+                          !!value.length ||
+                          "Please select at least one response",
+                  ]
+                : [],
+        };
     },
-    methods: {
-      onChange: function() {
-        console.log(this.sel)
-        this.$emit('responded', this.sel)
-      }
-    }
-  }
+    watch: {
+        sel: function() {
+            this.$emit("responded", this.sel);
+        },
+    },
+};
 </script>
--- src/components/controls/SingleChoiceInput.vue
+++ src/components/controls/SingleChoiceInput.vue
@@ -1,39 +1,54 @@
 <template>
-  <v-container fluid>
-    <div class="text-left">
-      <h2>{{label}}</h2>
-      <h3>Please select one choice</h3>
-    </div>
-    <v-item-group v-model="sel" @change="onChange" :rules="rules" :class="name">
-      <v-row dense>
-          <v-col v-for="choice in choices" :key="choice.value" :value="choice" cols="12">
-              <choice :value="choice" :index="choice" :label="choice.value"/>
-          </v-col>
-      </v-row>
-    </v-item-group>
-  </v-container>
+    <v-input :rules="rules" :value="sel">
+        <v-container fluid>
+            <div class="text-left">
+                <h2>{{ label }}</h2>
+                <h3>Please select one choice</h3>
+            </div>
+            <v-item-group v-model="sel" :class="name">
+                <v-row dense>
+                    <v-col
+                        v-for="choice in choices"
+                        :key="choice.value"
+                        cols="12"
+                    >
+                        <choice
+                            :value="choice"
+                            :index="choice"
+                            :label="choice.value"
+                        />
+                    </v-col>
+                </v-row>
+            </v-item-group>
+        </v-container>
+    </v-input>
 </template>
 
 <script>
-  import Choice from '@/components/controls/Choice.vue'
-  
-  export default {
-    name: 'SingleChoiceInput',
-    props: ['label', 'name', 'choices', 'isMandatory'],
+import Choice from "@/components/controls/Choice.vue";
+
+export default {
+    name: "SingleChoiceInput",
+    props: ["label", "name", "choices", "isMandatory"],
     components: {
-      Choice,
+        Choice,
     },
     data() {
         return {
             sel: {},
-            rules: this.isMandatory ? [choice => choice.value !== undefined || 'Please select a response'] : []
-        }
+            rules: this.isMandatory
+                ? [
+                      (choice) =>
+                          choice.value !== undefined ||
+                          "Please select a response",
+                  ]
+                : [],
+        };
     },
-    methods: {
-      onChange: function() {
-        console.log(this.sel)
-        this.$emit('responded', [this.sel])
-      }
-    }
-  }
+    watch: {
+        sel: function() {
+            this.$emit("responded", [this.sel]);
+        },
+    },
+};
 </script>
