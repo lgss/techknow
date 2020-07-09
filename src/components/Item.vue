c8815538edc0797da328db77a31e5603390c6504
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-07-09 20:13:00 +0100

    update to label element for screen reader

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -3,8 +3,8 @@
         <v-container max-width="1200" class="mx-auto">
             <div class="d-flex flex-wrap align-center">
                 <div class="text-left">
-                    <h2>{{ title }}</h2>
-                    <h3>{{ subtitle }}</h3>
+                    <label class="text-h3 mb-2" v-text="title"></label>
+                    <div class="font-weight-bold mb-0" v-text="subtitle"></div>
                 </div>
                 <v-spacer></v-spacer>
                 <v-avatar class="ma-3" size="125" tile>
