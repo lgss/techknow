d1f433b48e66c1fefcf324b40cc782921fd65777
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-07-15 17:41:44 +0100

    add attrivutes to elements for a11y and tests

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,13 +1,27 @@
 <template>  
-    <v-dialog persistent @input="change" @fullscreen="fullscreen" id="modal-dialog" v-model="dialog" width="500px">
-      <v-card>
-          <v-card-title>{{title}}</v-card-title>
-          <v-card-text>{{message}}</v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn role="button" v-for="(btn, index) in buttons" :key="index" text @click="choose(index)">{{btn}}</v-btn>
-          </v-card-actions>
-      </v-card>
+    <v-dialog persistent :input="change" :fullscreen="fullscreen" id="modal-dialog" v-model="dialog" width="500px">
+        <v-card>
+            <v-card-title 
+              id="dialog-title" 
+              role="heading" 
+              aria-level="3" 
+              class="text-h3 mb-2"
+              tabindex="0"
+            >
+              {{title}}
+            </v-card-title>
+            <v-card-text id="dialog-content">{{message}}</v-card-text>
+            <v-row justify="center">
+              <v-card-actions>
+                <v-btn 
+                  role="button" 
+                  v-for="(btn, index) in buttons" 
+                  :key="index" 
+                  text @click="choose(index)">{{btn}}
+                </v-btn>
+              </v-card-actions>
+            </v-row>
+        </v-card>
     </v-dialog>
 </template>
 
