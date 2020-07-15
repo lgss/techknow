9c69631e8d483c030ce4cc11846594b60f900345
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-07-15 17:41:03 +0100

    amended to get fullscreen working

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -3,6 +3,7 @@ import Dialog from './Dialog.vue'
 function Install (Vue, vuetify) {
     const property = '$dialog'
     const Ctor = Vue.extend(Object.assign({ vuetify }, Dialog))
+
     function createDialogCmp (options) {
       const container = document.querySelector('[data-app=true]') || document.body
       return new Promise(resolve => {
@@ -17,16 +18,17 @@ function Install (Vue, vuetify) {
       })
     }
     
-    function show (title, message, buttons, options = {}) {
+    function show (title, message, buttons, fullscreen = false, options = {}) {
       options.title = title
       options.message = message
       options.buttons = buttons
+      options.fullscreen = fullscreen
       return createDialogCmp(options)
     }
   
     Vue.prototype[property] = show  
     Vue.prototype[property].confirm = (title, message) => {return show(title, message, ['OK', 'Cancel'])}
-    Vue.prototype[property].fullscreen = (message) => {return show('', message, ['Back']), {fullscreen: true}}
+    Vue.prototype[property].fullscreen = (title, message) => {return show(title, message, ['Back'], true)}
   }
 
   if (typeof window !== 'undefined' && window.Vue) {
