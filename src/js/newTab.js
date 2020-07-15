8875133097be8664fc6eca0151d20e481c44a002
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-07-15 20:40:31 +0100

    Update newTab.js

--- src/js/newTab.js
+++ src/js/newTab.js
@@ -1,4 +1,4 @@
 const newTab = (string) => {
-    return string.replace(`<a href`,`<a target="_blank" href`)
-}
-export default newTab
+    return string.replace(`<a href`, `<a target="_blank" href`);
+};
+export default newTab;
