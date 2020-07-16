4daa69664c780aa4abf6a59204ccc2ae630f2c2b
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-07-16 13:36:31 +0100

    Use new display func

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -64,9 +64,10 @@ export default {
     methods: {
         startAgain() {
             this.$dialog
-                .confirm(
+                .display(
                     "Start again",
-                    "The resources currently shown will be lost. You will need to complete the assessment again from the beginning. Are you sure you want to start again?"
+                    "The resources currently shown will be lost. You will need to complete the assessment again from the beginning. Are you sure you want to start again?",
+                    ["Yes","No"]
                 )
                 .then((result) => {
                     if (result === 0) this.$router.push({ name: "Select" });
