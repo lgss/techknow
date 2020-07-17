617a0ff7662545b728c2d31fc9d67bc050285d14
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-07-17 15:45:54 +0100

    Update dialog layout, style and focus

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -2,30 +2,27 @@
     <v-dialog persistent :input="change" :fullscreen="fullscreen" id="modal-dialog" v-model="dialog" width="500px">
         <v-card>
             <v-card-title 
+              ref="dialog-title"
               id="dialog-title" 
               role="heading" 
               aria-level="3" 
               class="text-h3 mb-2"
               tabindex="0"
-            >
-              {{title}}
-            </v-card-title>
+              v-html="title"
+            ></v-card-title>
             <v-card-text 
               id="dialog-content"
               tabindex="0"
-            >
-              {{message}}
-            </v-card-text>
-            <v-row justify="center">
-              <v-card-actions>
-                <v-btn 
-                  role="button" 
-                  v-for="(btn, index) in buttons" 
-                  :key="index" 
-                  text @click="choose(index)">{{btn}}
-                </v-btn>
-              </v-card-actions>
-            </v-row>
+              v-html="message"
+            ></v-card-text>
+            <v-card-actions>
+              <v-btn 
+                role="button" 
+                v-for="(btn, index) in buttons" 
+                :key="index" 
+                text @click="choose(index)">{{btn}}
+              </v-btn>
+            </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
@@ -42,6 +39,11 @@ export default {
           type: Boolean,
           default: false
       }
+    },
+    created() {
+      this.$nextTick(()=> {
+          this.$refs[`dialog-title`].focus()
+      })
     },
     methods: {
       choose(value) {
@@ -60,3 +62,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+  .v-dialog__content {
+    background-color: rgba(33, 33, 33, 0.7);
+  }
+</style>
