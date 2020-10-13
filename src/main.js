2856bc7c1fad1548ae39cc9b181d30ba347db3c6
13enedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-10-13 11:03:42 +0100

    Added Google Analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
@@ -11,3 +12,9 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+if (process.env.VUE_APP_GOOGLE_ANALYTICS_ID)
+  Vue.use(VueAnalytics, {
+    id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+    router
+  })
