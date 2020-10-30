dea1516c35cbbf730daf5f3858ceed61e1a03227
13enedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-10-30 12:37:11 +0000

    Fixed setting gtag properties

--- src/main.js
+++ src/main.js
@@ -20,11 +20,12 @@ if (process.env.VUE_APP_GOOGLE_ANALYTICS_ID) {
     config: { id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID },
     appName: "rekommend",
     pageTrackerScreenviewEnabled: true,
+    onReady: (gtag) => {
+      const doTrack = Object.prototype.hasOwnProperty.call(localStorage, "ga_consent") && localStorage.ga_consent==="true"
+
+      gtag('set', 'allowAdFeatures', doTrack)
+      gtag('set', 'anonymizeIp', !doTrack)
+    },
     enabled: !Object.prototype.hasOwnProperty.call(localStorage, "ga_consent") || localStorage.ga_consent==="true",
   }, router);
-
-  const doTrack = Object.prototype.hasOwnProperty.call(localStorage, "ga_consent") && localStorage.ga_consent==="true"
-
-  this.$gtag.set('allowAdFeatures', doTrack)
-  this.$gtag.set('anonymizeIp', !doTrack)
 }
